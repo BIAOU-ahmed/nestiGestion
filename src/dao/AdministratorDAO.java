1393ce44797a617c5feb27413c0e6b461a7921fb
Ahmed <ahmed.alidjinou@hotmail.com>
2021-01-15 18:58:32 +0100

    update Administrator DAO

--- src/dao/AdministratorDAO.java
+++ src/dao/AdministratorDAO.java
@@ -3,9 +3,11 @@
  */
 package dao;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import model.Administrator;
+import model.Product;
 import tools.DBConnection;
 
 /**
@@ -20,4 +22,24 @@ public class AdministratorDAO extends BaseDAO<Administrator> {
 
 		return "administrator";
 	}
+	
+	
+	@Override
+	 public Administrator getFromResultSet(ResultSet rs) throws SQLException
+	    {
+		Administrator result = null;
+
+	        if (rs != null && !rs.isClosed() && rs.next()) {
+	        	
+	        	result = new Administrator();
+	        	result.setId(rs.getInt("idAdministrator"));
+	        	result.setUserName(rs.getString("userName "));
+	        	result.setAdminState(rs.getString("adminState "));
+	        	result.setAdminState(rs.getString("adminState "));
+		        	
+	        	
+	        }
+	        
+	        return result;
+	    }
 }
