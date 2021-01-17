7995ccad8550131b0e2312909d2816c47022dc23
Ahmed <ahmed.alidjinou@hotmail.com>
2021-01-17 11:09:52 +0100

    create a DAO to add utensils

--- src/dao/UtensilDAO.java
+++ src/dao/UtensilDAO.java
@@ -40,12 +40,23 @@ public class UtensilDAO extends BaseDAO<Utensil> {
 //	        return result;
 //	    }
 	 
-	public void insert (Utensil ing) throws SQLException {
-        var sql = "INSERT INTO"+ getTableName() +"(`idProduct`) VALUES (?,?);"; // Don't insert ID, let database auto-increment it.
+	public void insert (Utensil utensil) throws SQLException {
+        var sql = "INSERT INTO "+ getTableName() +"(`idProduct`) VALUES (?);"; // Don't insert ID, let database auto-increment it.
 
         var insertUtensil = DBConnection.get().prepareStatement(sql);
      
-        insertUtensil.setInt(1, ing.getId());
+        insertUtensil.setInt(1, utensil.getId());
+        
+        insertUtensil.executeUpdate();
+  
+    }
+	
+	public void delete (Utensil utensil) throws SQLException {
+        var sql = "DELETE FROM `utensil` WHERE  	idProduct = ?"; // Don't insert ID, let database auto-increment it.
+
+        var insertUtensil = DBConnection.get().prepareStatement(sql);
+     
+        insertUtensil.setInt(1, utensil.getId());
         
         insertUtensil.executeUpdate();
   
